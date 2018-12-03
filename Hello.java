57a7cc5f851c3b34ac7563cefd53caadb827ad52
litwave <litwave@kosaf.go.kr>
2018-12-03 17:48:17 +0900

    Hello.java modified on Local repository

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String [] args) {
+		// For command line git tutorial
+		// Local repository modification
 		System.out.println("Hello World!!");
 		System.out.println("Hello Litwave!!");
 		System.out.println("Hello nemonemo!!");
